1c45062d00e2507b5457db9f2d435ce1eb68211d
YK115 <kurzhumov@gmail.com>
2014-06-03 14:31:42 +0600

    Update and rename SmartSystemsAjaxBundle.php to SmartInformationSystemsAjaxBundle.php

---
+++ SmartInformationSystemsAjaxBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SmartInformationSystems\AjaxBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SmartInformationSystemsAjaxBundle extends Bundle
+{
+}
--- SmartSystemsAjaxBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SmartSystems\AjaxBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SmartSystemsAjaxBundle extends Bundle
-{
-}
