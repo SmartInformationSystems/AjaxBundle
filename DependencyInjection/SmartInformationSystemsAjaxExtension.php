593c53e7e3cde398dc22590ed2aae280d96045a7
YK115 <kurzhumov@gmail.com>
2014-06-03 14:33:22 +0600

    Update and rename SmartSystemsAjaxExtension.php to SmartInformationSystemsAjaxExtension.php

--- DependencyInjection/SmartInformationSystemsAjaxExtension.php
+++ DependencyInjection/SmartInformationSystemsAjaxExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SmartSystems\AjaxBundle\DependencyInjection;
+namespace SmartInformationSystems\AjaxBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SmartSystemsAjaxExtension extends Extension
+class SmartInformationSystemsAjaxExtension extends Extension
 {
     /**
      * {@inheritDoc}
